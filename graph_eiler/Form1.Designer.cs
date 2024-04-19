14bd23171cf67716ce7b70c1c7d6613f7c9883e1
tel9m <tel9m@DESKTOP-IEM3UBD>
2024-04-19 20:01:51 +0300

    Добавьте файлы проекта.

---
+++ graph_eiler/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace graph_eiler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(135, 448);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Найти эйлеровый путь";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(12, 532);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(449, 20);
+            this.txtResult.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(449, 397);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(194, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Граф:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(152, 509);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Эйлеровый путь:";
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(227, 448);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 49);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Закрыть";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(206, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(356, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(408, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "3";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(356, 326);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(15, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "4";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(206, 400);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(15, 16);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "5";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(74, 230);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(15, 16);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "7";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(104, 128);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(15, 16);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "8";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(104, 326);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(15, 16);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(470, 561);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Эйлеровый путь";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ graph_eiler/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace graph_eiler
+{
+    public partial class Form1 : Form
+    {
+        // Матрица смежности для графа
+        public double[,] graph = new double[8, 8]
+        {
+            { 0, 3, 0, 5, 6, 4, 0, 6 },
+            { 3, 0, 3, 0, 0, 4, 0, 5 },
+            { 0, 3, 0, 2, 0, 0, 0, 0 },
+            { 5, 0, 2, 0, 2, 0, 0, 0 },
+            { 6, 0, 0, 2, 0, 1, 0, 7 },
+            { 4, 4, 0, 0, 1, 0, 1, 0 },
+            { 0, 0, 0, 0, 0, 1, 0, 5 },
+            { 6, 5, 0, 0, 7, 0, 5, 0 }
+        };
+
+        private Dictionary<int, Point> nodeCoordinates = new Dictionary<int, Point>
+        {
+            { 1, new Point(200, 50) },
+            { 2, new Point(350, 100) },
+            { 3, new Point(400, 200) },
+            { 4, new Point(350, 300) },
+            { 5, new Point(200, 370) },
+            { 6, new Point(100, 300) },
+            { 7, new Point(70, 200) },
+            { 8, new Point(100, 100) }
+        };
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+            pictureBox1.Paint += pictureBox1_Paint;
+            DrawGraph(pictureBox1.CreateGraphics());
+        }
+        private void DrawGraph(Graphics g)
+        {
+            // Очистим pictureBox перед рисованием
+            g.Clear(Color.White);
+            // Радиус вершин
+            int radius = 20;
+
+            // Рисуем вершины
+            foreach (var kvp in nodeCoordinates)
+            {
+                int node = kvp.Key;
+                Point center = kvp.Value;
+                g.FillEllipse(Brushes.LightBlue, center.X - radius, center.Y - radius, 2 * radius, 2 * radius);
+                g.DrawEllipse(Pens.Black, center.X - radius, center.Y - radius, 2 * radius, 2 * radius);
+                g.DrawString(node.ToString(), new Font("Arial", 12), Brushes.Black, center.X - 8, center.Y - 8);
+            }
+
+            // Рисуем рёбра
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                for (int j = i + 1; j < graph.GetLength(1); j++)
+                {
+                    if (graph[i, j] > 0)
+                    {
+                        Point start = nodeCoordinates[i + 1];
+                        Point end = nodeCoordinates[j + 1];
+                        g.DrawLine(Pens.Black, start, end);
+                        // Рисуем вес ребра
+                        g.DrawString(graph[i, j].ToString(), new Font("Arial", 10), Brushes.Black, (start.X + end.X) / 2, (start.Y + end.Y) / 2);
+                    }
+                }
+            }
+        }
+        private List<int> FindEulerPath()
+        {
+            // Список для хранения пути
+            List<int> path = new List<int>();
+            // Стек для обхода графа
+            Stack<int> stack = new Stack<int>();
+            // Создание копии матрицы смежности для графа
+            double[,] tempGraph = (double[,])graph.Clone();
+            // Стартовая вершина
+            int startNode = 0;
+            stack.Push(startNode);
+
+            // Подсчет степени каждой вершины
+            int[] degrees = new int[graph.GetLength(0)];
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                for (int j = 0; j < graph.GetLength(1); j++)
+                {
+                    if (graph[i, j] > 0)
+                    {
+                        degrees[i]++;
+                    }
+                }
+            }
+
+            while (stack.Count > 0)
+            {
+                int currentNode = stack.Peek();
+                int i;
+
+                for (i = 0; i < graph.GetLength(0); i++)
+                {
+                    if (tempGraph[currentNode, i] > 0)
+                    {
+                        // Удаляем ребро из графа
+                        tempGraph[currentNode, i] = 0;
+                        tempGraph[i, currentNode] = 0;
+                        stack.Push(i);
+                        break;
+                    }
+                }
+
+                if (i == graph.GetLength(0))
+                {
+                    // Если у вершины больше нет инцидентных рёбер, добавляем её в путь
+                    path.Add(currentNode);
+                    stack.Pop();
+                }
+            }
+
+            // Проверка на чётность степени каждой вершины
+            for (int i = 0; i < degrees.Length; i++)
+            {
+                if (degrees[i] % 2 != 0)
+                {
+                    MessageBox.Show($"Граф не имеет эйлерова пути, так как у вершины {i + 1} нечётная степень.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return null;
+                }
+            }
+
+            // Путь обходит каждое ребро ровно один раз, поэтому нужно перевернуть путь и прибавить 1 к каждой вершине
+            path.Reverse();
+            for (int i = 0; i < path.Count; i++)
+            {
+                path[i]++;
+            }
+
+            // Проверяем, что путь замкнутый (начальная и конечная вершины совпадают)
+            if (path[0] != path[path.Count - 1])
+            {
+                MessageBox.Show("Граф не является замкнутым.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+
+            return path;
+        }
+
+        private void DrawGraphWithDelay(List<int> eulerPath)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            DrawGraph(g); // Начальное отображение графа
+
+            // Получаем количество вершин в графе
+            int numVertices = nodeCoordinates.Count;
+
+            // Словарь для отслеживания цветов вершин
+            Dictionary<int, Color> nodeColors = new Dictionary<int, Color>();
+
+            // Список перьев для рёбер
+            List<Pen> edgePens = new List<Pen>();
+
+            // Определяем количество различных цветов, которые будут использованы для раскраски вершин и рёбер
+            int numNodeColors = (numVertices % 2 == 0) ? numVertices / 2 : (numVertices + 1) / 2;
+            int numEdgeColors = (numVertices % 2 == 0) ? numVertices / 2 : (numVertices - 1) / 2;
+
+            // Генерируем цвета для вершин
+            List<Color> nodeColorList = GenerateColors(numNodeColors);
+            // Генерируем цвета для рёбер
+            List<Color> edgeColorList = GenerateColors(numEdgeColors * 4);
+
+
+            // Создаем перья для всех возможных рёбер в графе
+            foreach (Color color in edgeColorList)
+            {
+                edgePens.Add(new Pen(color, 5));
+            }
+
+            // Радиус вершин
+            int radius = 20;
+
+            // Отрисовка с задержкой
+            for (int i = 0; i < eulerPath.Count - 1; i++)
+            {
+                int node1 = eulerPath[i] - 1;
+                int node2 = eulerPath[i + 1] - 1;
+
+                // Помечаем текущее ребро
+                if (i < edgePens.Count) // Проверяем, что есть соответствующее перо для текущего ребра
+                {
+                    DrawEdge(g, node1, node2, edgePens[i]);
+
+                    // Задержка в 1 секунду
+                    Thread.Sleep(1000);
+
+                    // Убираем пометку с ребра
+                    DrawEdge(g, node1, node2, Pens.Black);
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка: количество рёбер в графе превышает количество цветов рёбер. Пожалуйста, увеличьте количество цветов или уменьшите количество рёбер.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                // Проверяем, был ли уже окрашен данный узел
+                if (!nodeColors.ContainsKey(node1))
+                {
+                    // Окрашиваем вершину
+                    g.FillEllipse(new SolidBrush(nodeColorList[node1 % nodeColorList.Count]), nodeCoordinates[node1 + 1].X - radius, nodeCoordinates[node1 + 1].Y - radius, 2 * radius, 2 * radius);
+                    nodeColors.Add(node1, nodeColorList[node1 % nodeColorList.Count]);
+                }
+
+                if (!nodeColors.ContainsKey(node2))
+                {
+                    // Окрашиваем вершину
+                    g.FillEllipse(new SolidBrush(nodeColorList[node2 % nodeColorList.Count]), nodeCoordinates[node2 + 1].X - radius, nodeCoordinates[node2 + 1].Y - radius, 2 * radius, 2 * radius);
+                    nodeColors.Add(node2, nodeColorList[node2 % nodeColorList.Count]);
+                }
+
+
+            }
+
+            // Освобождаем ресурсы перьев рёбер
+            foreach (Pen pen in edgePens)
+            {
+                pen.Dispose();
+            }
+        }
+        private void DrawEdge(Graphics g, int node1, int node2, Pen pen)
+        {
+            Point start = nodeCoordinates[node1 + 1];
+            Point end = nodeCoordinates[node2 + 1];
+            g.DrawLine(pen, start, end);
+            // Рисуем вес ребра
+            g.DrawString(graph[node1, node2].ToString(), new Font("Arial", 10), Brushes.Black, (start.X + end.X) / 2, (start.Y + end.Y) / 2);
+        }
+
+        private List<Color> GenerateColors(int numColors)
+        {
+            // Генерируем указанное количество различных случайных цветов
+            List<Color> colors = new List<Color>();
+            Random rand = new Random();
+            for (int i = 0; i < numColors; i++)
+            {
+                colors.Add(Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256)));
+            }
+            return colors;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Находим эйлеров путь
+            List<int> eulerPath = FindEulerPath();
+            txtResult.Text = string.Join(" -> ", eulerPath);
+
+            // Отрисовываем граф с задержкой
+            DrawGraphWithDelay(eulerPath);
+        }
+        private void Form1_Load(object sender, EventArgs e) { DrawGraph(pictureBox1.CreateGraphics()); }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e) { DrawGraph(e.Graphics); }
+        private void button2_Click(object sender, EventArgs e) { Application.Exit(); }
+    }
+}
